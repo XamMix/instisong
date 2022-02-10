89ddc0f06940b1e045dc1600fde8e3c87337e168
XamMix <62408960+XamMix@users.noreply.github.com>
2022-02-10 08:49:27 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -2,13 +2,19 @@ function doGet() {
   const html = HtmlService.createTemplateFromFile('indexInstituciones');
   const output = html.evaluate();
   return output;
-}
+} 
 
 function getDataInstituciones() {
   const SS = SpreadsheetApp.getActiveSpreadsheet();
   const sheetLISTADO_DE_INSTI = SS.getSheetByName('LISTADO DE INSTI');
   const dataInstituciones = sheetLISTADO_DE_INSTI.getDataRange().getDisplayValues();
   dataInstituciones.shift();
+  fetch('https://sheet.best/api/sheets/f169a08e-cd34-4ea2-bc0d-21ad75736277');
+  method: 'POST',
+  mode: 'cors',
+  headers: {
+    'Content-Type': 'application/json'
+  }
   
   //console.log(dataInstituciones);
 

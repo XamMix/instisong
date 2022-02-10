e785b231e030b969e3281fd3a640157d7b92701d
XamMix <62408960+XamMix@users.noreply.github.com>
2022-02-10 08:14:06 -0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,11 @@
+<script>
+function darkmode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+
+      function uiprint() { 
+        window.print(); 
+      }
+</script>
